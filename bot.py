1e1872c355de64f0ca7244277fc4bd130d7743c2
Danielius <vkanada6417@gmail.com>
2024-11-24 11:53:44 +0300

    Добавлен хэндлер для банов за ссылки

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-import telebot # библиотека telebot
-from config import token # импорт токена
+import telebot
+from config import token
 
-bot = telebot.TeleBot(token) 
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -9,18 +9,43 @@ def start(message):
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
+    if message.reply_to_message:
+        chat_id = message.chat.id
         user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+        user_status = bot.get_chat_member(chat_id, user_id).status
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    help_text = """
+Бот для управления чатом.
+
+Команды:
+
+* `/start` - начать работу с ботом.
+* `/ban` - банить пользователя, отправившего сообщение. Используйте эту команду в ответ на сообщение пользователя, которого вы хотите забанить.
+* `/ban` автоматически банит пользователей, которые отправляют ссылки в группе.
+
+Примечание: Бот будет работать только в группах, где он имеет права администратора.
+"""
+    bot.reply_to(message, help_text)
+
+@bot.message_handler(func=lambda message: True)
+def check_message(message):
+    if "https://" in message.text:
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь @{message.from_user.username} был забанен за отправку ссылки.")
 
 bot.infinity_polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = "" # токен для бота
+token = "7648545321:AAH90aZm2tU-tu2lKl5x8R7Nv5OxmZUpz6o" # токен для бота
